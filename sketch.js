8719389deffe7db1d7bde2eb6ad34fab91b1a0ae
unknown <navalepoorva@gmail.com>
2020-11-11 16:48:41 +0530

    created log

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +15,8 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
+    log = new log (250,450,50,50);
 
 
     ground = new Ground(600,height,1200,20);
